e1b5ea6815aaeaf5d256b09b2645e75a27b6139f
Gert Peter Rue Brigsted <gpb@kmd.dk>
2022-05-24 11:02:55 +0200

    Initial commit.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd05366fec38054cabb070bd54ff411c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f53497f6-dac6-4fb4-9f0b-be63afc3e10b'
+};
